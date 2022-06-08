7ba4b108feb858774bc7640af82470feb40d2440
mayu nishikawa <sheseesseaontheseashore@gmail.com>
2022-06-08 10:04:04 +0900

    add user registration system

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
+  root 'sessions#new'
   resources :pictures
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :users, only: [:new, :create, :show]
 end
