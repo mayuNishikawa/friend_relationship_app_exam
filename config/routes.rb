7ebb4ca5a3e9c937b322ed167ccfbd65d9e59b81
mayu nishikawa <sheseesseaontheseashore@gmail.com>
2022-06-09 14:19:09 +0900

    add session

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   root 'sessions#new'
   resources :pictures
   resources :users, only: [:new, :create, :show]
+  resources :sessions, only: [:new, :create, :destroy]
 end
